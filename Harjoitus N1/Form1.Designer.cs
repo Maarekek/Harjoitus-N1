f0311e8ba01f88b41ca541ec592e470d5d6791ba
Maarek Mossin <maarek.mossin@edu.keuda.fi>
2025-09-18 13:01:42 +0300

    Add project files.

---
+++ Harjoitus N1/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Harjoitus_N1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OtsikkoLB = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // OtsikkoLB
+            // 
+            this.OtsikkoLB.AutoSize = true;
+            this.OtsikkoLB.Location = new System.Drawing.Point(15, 9);
+            this.OtsikkoLB.Name = "OtsikkoLB";
+            this.OtsikkoLB.Size = new System.Drawing.Size(47, 15);
+            this.OtsikkoLB.TabIndex = 0;
+            this.OtsikkoLB.Text = "Otsikko";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(27, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 56);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Vaihda teksti";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(593, 342);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.OtsikkoLB);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label OtsikkoLB;
+        private Button button1;
+    }
+}
